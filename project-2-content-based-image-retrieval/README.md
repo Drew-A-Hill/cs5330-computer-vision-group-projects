f0018212e79eea2553741c7ef6ca854347533769
Drew-A-Hill <hill.dr@northeastern.edu>
2026-06-06 02:23:15 -0400

    Complete Project

--- project-2-content-based-image-retrieval/README.md
+++ project-2-content-based-image-retrieval/README.md
@@ -1,2 +1,48 @@
-# cs5330-computer-vision-group-projects
-Repo used for cs3350 group projects
+# Project 2: Contnet Based Image Retrieval
+**Drew Hill, Abhiram Banda**    
+CS5330 Pattern Recognition & Computer Vision
+
+
+## Files Submitted
+matcher.cpp: Main matching logic.
+csv_helper.cpp: Generates the CSV files for features. 
+features.cpp: Feature extraction functions.
+features.h: Header file for features functions.
+features_helper.cpp: Computes sobel x sobel y and magnitude.
+features_helper.h: the header file for the features_helper functions.
+
+## Build
+To run this program you need to compile both make and csv_helper then you must run the programs with required arguments. For all except designated you must first must run csv_helper to generate a csv of feature values then run matcher. Below will provide compile and run instructions for each.  
+
+**CSV_Helper**
+Compile with: make csv_helper  
+Run with the following:
+./csv_helper olympus features.csv b  
+./csv_helper olympus histogram_features.csv h    
+./csv_helper olympus histogram_features_3.csv h3  
+./csv_helper olympus mult_hist_1.csv mh  
+./csv_helper olympus texture.csv tx  
+
+
+**Matcher:**  
+Compile with: make matcher  
+Run with: ./matcher image_path csv_file N method 
+
+**Methods:**  
+  b    - Baseline  
+  h    - Color histogram matching  
+  h3   - 3D RGB histogram matching  
+  mh   - Multi histogram matching  
+  mh2  - Full and center histogram matching  
+  tx   - Texture and color matching  
+  rc   - DNN cosine distance   
+  dvf  - Compare DNN vs classic  
+  lp   - Custom flower matching  
+  res2 - DNN SSD distance (Extension 1)   
+
+## Extensions
+**Extension1 (res2):** Uses DNN embeding with SSD distance metric rather than using the cosine distance metric. Run with te csv file path ResNet18_olym.csv and method res2. The different outcomes are then compared between using SSD and cosine difference.  
+
+**Extension 2 (GUI):** All methods will automatically display the target image and the top matches in a window. Press any key to close the window.
+
+
--- project-2-content-based-image-retrieval/matcher.cpp
+++ project-2-content-based-image-retrieval/matcher.cpp
@@ -1,14 +1,13 @@
 /*
-    Abhiram Banda, Drew Hill
-    Summer 2026
-    CS 5330 Computer Vision
+  Abhiram Banda, Drew Hill
+  Summer 2026
+  CS 5330 Computer Vision
 
   matcher.cpp
 
   This file is used to compare the target image to the other images in the database and provide an
   ordered vector of images that are most similar to the target image.
 */
-
 #include <cstdio>
 #include <cstring>
 #include <vector>
@@ -28,12 +27,12 @@ namespace fs = std::filesystem;
   This method computes the baseline feature diffence between the target image and each of the other
   images in the data base.
 
-  cv::Mat targetImg the target image.
-
+  cv::Mat targetImg a Mat containging the target image.
+  std::vector<char *> all filenames in the data bases being used for comparisons.
+  std::vector<std::vector<float>> data a vector that contains each vector of data for each image.
+  returns std::vector<std::pair<float, std::string>> a vector of pairs containing the distance to target and filename.
 */
-std::vector<std::pair<float, std::string>> baselineMatching(
-    cv::Mat targetImg, std::vector<char *> filenames, 
-    std::vector<std::vector<float>> data) {
+std::vector<std::pair<float, std::string>> baselineMatching(cv::Mat targetImg, std::vector<char *> filenames, std::vector<std::vector<float>> data) {
 
     // Computes the features for the target image returned as a vector.
     std::vector<float> targetImgVector = extractBaselineFeature(targetImg);
@@ -58,6 +57,11 @@ std::vector<std::pair<float, std::string>> baselineMatching(
 
 /*
   Task 2: Histogram Matching
+
+  cv::Mat targetImg a Mat containging the target image.
+  std::vector<char *> all filenames in the data bases being used for comparisons.
+  std::vector<std::vector<float>> data a vector that contains each vector of data for each image.
+  returns std::vector<std::pair<float, std::string>> a vector of pairs containing the distance to target and filename.
 */
 std::vector<std::pair<float, std::string>>histMatching(cv::Mat targetImg, std::vector<char *> filenames, std::vector<std::vector<float>> data) {
 
@@ -86,6 +90,11 @@ std::vector<std::pair<float, std::string>>histMatching(cv::Mat targetImg, std::v
 
 /*
   Task 2: Histogram matching 3x3 histogram.
+
+  cv::Mat targetImg a Mat containging the target image.
+  std::vector<char *> all filenames in the data bases being used for comparisons.
+  std::vector<std::vector<float>> data a vector that contains each vector of data for each image.
+  returns std::vector<std::pair<float, std::string>> a vector of pairs containing the distance to target and filename.
 */
 std::vector<std::pair<float, std::string>> hist3x3Matching(cv::Mat targetImg, std::vector<char *> filenames, std::vector<std::vector<float>> data) {
     // Computes 3x3 histogram.
@@ -113,6 +122,11 @@ std::vector<std::pair<float, std::string>> hist3x3Matching(cv::Mat targetImg, st
 
 /*
   Task 3: Multiple Histogram Matching
+
+  cv::Mat targetImg a Mat containging the target image.
+  std::vector<char *> all filenames in the data bases being used for comparisons.
+  std::vector<std::vector<float>> data a vector that contains each vector of data for each image.
+  returns std::vector<std::pair<float, std::string>> a vector of pairs containing the distance to target and filename.
 */
 std::vector<std::pair<float, std::string>> multiHistMatching(cv::Mat targetImg, std::vector<char *> filenames, std::vector<std::vector<float>> data) {
     //holds the vector for the targetImage whcih we'll use to compare with each image
@@ -160,6 +174,10 @@ std::vector<std::pair<float, std::string>> multiHistMatching(cv::Mat targetImg, 
 /*
   Task 3: Multi Histogram Matching Part 2.
 
+  cv::Mat targetImg a Mat containging the target image.
+  std::vector<char *> all filenames in the data bases being used for comparisons.
+  std::vector<std::vector<float>> data a vector that contains each vector of data for each image.
+  returns std::vector<std::pair<float, std::string>> a vector of pairs containing the distance to target and filename.
 */
 std::vector<std::pair<float, std::string>> multiHistFullMiddleMatch(
     cv::Mat targetImg, 
@@ -195,6 +213,11 @@ std::vector<std::pair<float, std::string>> multiHistFullMiddleMatch(
 
 /*
   Task 4: Texture and Color
+
+  cv::Mat targetImg a Mat containging the target image.
+  std::vector<char *> all filenames in the data bases being used for comparisons.
+  std::vector<std::vector<float>> data a vector that contains each vector of data for each image.
+  returns std::vector<std::pair<float, std::string>> a vector of pairs containing the distance to target and filename.
 */
 std::vector<std::pair<float, std::string>> texturedMatching(cv::Mat targetImg, std::vector<char *> filenames, std::vector<std::vector<float>> data) {
     //Compute target image texture features
@@ -266,6 +289,13 @@ static int normalizeV(std::vector<float> srcVector, std::vector<float> &dstVecto
 
 /*
   Task 5: Deep Network embedding
+  Function is used to compute the similar images vector using a dnn embedding. Uses cosign difference to compute
+  the difference between an image and the target image.
+
+  std::string imgArg the file name for the target image.
+  std::vector<char *> all filenames in the data bases being used for comparisons.
+  std::vector<std::vector<float>> data a vector that contains each vector of data for each image.
+  returns std::vector<std::pair<float, std::string>> a vector of pairs containing the distance to target and filename.
 */
 std::vector<std::pair<float, std::string>> dnnMatching(std::string imgArg, std::vector<char *> filenames, std::vector<std::vector<float>> data) {
     std::vector<float> targetV;
@@ -280,7 +310,7 @@ std::vector<std::pair<float, std::string>> dnnMatching(std::string imgArg, std::
         } 
     }
 
-    // Mormalizes the target vector
+    // Normalizes the target vector
     normalizeV(targetV, targetV);
 
     // Declare a new vector of pairs containing the distance to target and filename.
@@ -322,6 +352,12 @@ std::vector<std::pair<float, std::string>> dnnMatching(std::string imgArg, std::
   Combines center-focused color histogram, texture histogram, and DNN embeddings.
   Flowers have distinctive bright colors (especially in the center of the image),
   petal texture patterns, and green foliage backgrounds.
+
+  cv::Mat targetImg a Mat containging the target image.
+  std::string imgArg the file name for the target image.
+  std::vector<char *> all dnnFilenames in the data bases being used for comparisons.
+  std::vector<std::vector<float>> dnnData a vector that contains each vector of data for each image.
+  returns std::vector<std::pair<float, std::string>> a vector of pairs containing the distance to target and filename.
 */
 std::vector<std::pair<float, std::string>> customMatching(
     cv::Mat targetImg, std::string imgName,
@@ -385,7 +421,82 @@ std::vector<std::pair<float, std::string>> customMatching(
 }
 
 /*
-  Prints the N file names with the smallest distance to target.
+  Extension:
+  Function is used to compute the similar images vector using a dnn embedding. Uses sum of squared 
+  differences difference to compute the difference between an image and the target image.
+
+  std::string imgArg the file name for the target image.
+  std::vector<char *> all filenames in the data bases being used for comparisons.
+  std::vector<std::vector<float>> data a vector that contains each vector of data for each image.
+  returns std::vector<std::pair<float, std::string>> a vector of pairs containing the distance to target and filename. 
+*/
+std::vector<std::pair<float, std::string>> dnnSSDMatching(std::string imgArg, std::vector<char *> filenames, std::vector<std::vector<float>> data) {
+    std::vector<float> targetV;
+
+    // Finds the target image vector in the csv file
+    for (int i = 0; i < filenames.size(); i++) {
+        if (strcmp(filenames[i], imgArg.c_str()) == 0) {
+            targetV = data[i];
+
+            printf("\nTarget %s Found\n\n", imgArg.c_str());
+            break;
+        } 
+    }
+
+    // Declare a new vector of pairs containing the distance to target and filename.
+    std::vector<std::pair<float, std::string>> pairs;
+
+    for (int i = 0; i < filenames.size(); i++) {
+
+        // Checks that img vector being compared is not the target img vector.
+        if (strcmp(filenames[i], imgArg.c_str()) != 0) {
+            // Computes the sum of squared differnces
+            float sumSD = ssd(targetV, data[i]);
+
+            // Adds sdd value and filename to the pair
+            std::pair<float, std::string> currPair(sumSD, filenames[i]);
+
+            // Adds the above pair to a vector
+            pairs.push_back(currPair);
+        }
+    }
+    // Sorts the vector of pairs.
+    std::sort(pairs.begin(), pairs.end());
+
+    return pairs;
+}
+
+/*
+  Extension2
+  This function builds a GUI that displays the target image, the N most similar images, 
+  as well as each of their distance values.
+
+  std::string filepath the file path of the target image to be displayed.
+  std::vector<std::pair<float, std::string>> pairs a vector containing paris with distance and filename.
+  int N the number of images in addition to the target image to display.
+*/
+void gui(std::string filepath, std::vector<std::pair<float, std::string>> pairs, int N) {
+    // Reads target image.
+    cv::Mat targetImg = cv::imread(filepath);
+
+    // Displays target image.
+    cv::imshow("Target - " + filepath, targetImg);
+
+    for (int i = 0; i < N; i++) {
+        // Reads the Nth most similar image file.
+        cv::Mat image = cv::imread("olympus/" + pairs[i].second);
+
+        // Builds window title from file name and distance.
+        std::string title = pairs[i].second + " (" + std::to_string(pairs[i].first) + ")";
+
+        // Displays the Nth mosth similar image.
+        cv::imshow(title, image);
+    }
+    cv::waitKey(0);
+}
+
+/*
+  Helper function that prints the N file names with the smallest distance to target.
 
   int N the number of filenames to print.
 */
@@ -396,7 +507,8 @@ void printFilenames(int N, std::vector<std::pair<float, std::string>> pairs) {
 }
 
 /*
-  TEMP NOTE: run ./matcher olympus/{img name.jpg} features.csv {N top imgs to display} action (ie tx for texture)
+  Main function that takes arguments to control image comparisons. 
+  run ./matcher olympus/{img name.jpg} features.csv {N top imgs to display} action (ie tx for texture)
 */
 int main(int argc, char* argv[]){
     std::string imgArg = argv[1];
@@ -416,10 +528,13 @@ int main(int argc, char* argv[]){
     int N = std::atoi(argv[3]);
     std::string method = argv[4];
 
+    // Declares a vector of pairs containing distance and filename.
+    std::vector<std::pair<float, std::string>> pairs;
+
     // Task 1: Baseline Matching
     if(method == "b"){
         // Computes distances and returns a pair of distances and filenames.
-        std::vector<std::pair<float, std::string>> pairs = baselineMatching(targetImg, filenames, data);
+        pairs = baselineMatching(targetImg, filenames, data);
 
         // Prints the N file names with the smallest distance to target.
         printFilenames(N, pairs);
@@ -428,7 +543,7 @@ int main(int argc, char* argv[]){
     // Task 2: Histogram Matching
     else if(method == "h"){
         // Computes distances and returns a pair of distances and filenames.
-        std::vector<std::pair<float, std::string>> pairs = histMatching(targetImg, filenames, data);
+        pairs = histMatching(targetImg, filenames, data);
         
         // Prints the N file names with the smallest distance to target.
         printFilenames(N, pairs);
@@ -437,7 +552,7 @@ int main(int argc, char* argv[]){
     // Task 2: 3x3 histogram matching.
     else if (method == "h3"){
         // Computes distances and returns a pair of distances and filenames.
-        std::vector<std::pair<float, std::string>> pairs = hist3x3Matching(targetImg, filenames, data);
+        pairs = hist3x3Matching(targetImg, filenames, data);
         
         // Prints the N file names with the smallest distance to target.
         printFilenames(N, pairs);
@@ -447,7 +562,7 @@ int main(int argc, char* argv[]){
     //Task 3: Multiple Histogram Matching
     else if (method == "mh") {
         // Computes distances and returns a pair of distances and filenames.
-        std::vector<std::pair<float, std::string>> pairs = multiHistMatching(targetImg, filenames, data);
+        pairs = multiHistMatching(targetImg, filenames, data);
         
         // Prints the N file names with the smallest distance to target.
         printFilenames(N, pairs);
@@ -456,7 +571,7 @@ int main(int argc, char* argv[]){
     //Task 3: Multiple Histogram Matching - pass 'mh2' as the method
     else if (method == "mh2"){
         // Computes distances and returns a pair of distances and filenames.
-        std::vector<std::pair<float, std::string>> pairs = multiHistFullMiddleMatch(targetImg, filenames, data);
+        pairs = multiHistFullMiddleMatch(targetImg, filenames, data);
         
         // Prints the N file names with the smallest distance to target.
         printFilenames(N, pairs);
@@ -465,7 +580,7 @@ int main(int argc, char* argv[]){
     //Task 4: Texture and Color
     else if (method == "tx") {
         // Computes distances and returns a pair of distances and filenames.
-        std::vector<std::pair<float, std::string>> pairs = texturedMatching(targetImg, filenames, data);
+        pairs = texturedMatching(targetImg, filenames, data);
         
         // Prints the N file names with the smallest distance to target.
         printFilenames(N, pairs);
@@ -474,18 +589,85 @@ int main(int argc, char* argv[]){
     // Task 5: Deep Network embedding
     else if (method == "rc") {
         // Computes distances and returns a pair of distances and filenames.
-        std::vector<std::pair<float, std::string>> pairs = dnnMatching(imgArg, filenames, data);
+        pairs = dnnMatching(imgArg, filenames, data);
         
         // Prints the N file names with the smallest distance to target.
         printFilenames(N, pairs);
-
-       
     } 
     
     // Task 6: Compare DNN Embedding and Classic Features
     else if (method == "dvf") {
+        // Declares vectors that hold all of the data from each features csv file.
+        std::vector<std::vector<float>> dnnData; 
+        std::vector<std::vector<float>> baselineData;
+        std::vector<std::vector<float>> histData;
+        std::vector<std::vector<float>> hist3x3Data;
+        std::vector<std::vector<float>> multiHistData;
+        std::vector<std::vector<float>> textureData;
+
+        // Declares vectors that hold the file names for each features csv file.
+        std::vector<char *> dnnFilenames;
+        std::vector<char *> baselineFilenames;
+        std::vector<char *> histFilenames;
+        std::vector<char *> hist3x3Filenames;
+        std::vector<char *> multiHistFilenames;
+        std::vector<char *> textureFilenames;
+
+        // Reads the csv and assigns the data and filenames to the correct variable declared above.
+        read_image_data_csv( (char*)"ResNet18_olym.csv", dnnFilenames, dnnData);
+        read_image_data_csv((char*)"features.csv", baselineFilenames, baselineData);
+        read_image_data_csv((char*)"histogram_features.csv", histFilenames, histData);
+        read_image_data_csv((char*)"histogram_features_3.csv", hist3x3Filenames, hist3x3Data);
+        read_image_data_csv((char*)"mult_hist_1.csv", multiHistFilenames, multiHistData);
+        read_image_data_csv((char*)"texture.csv", textureFilenames, textureData);
+        
+        // Declares variables to hold compared vectors of pairs.
+        std::vector<std::pair<float, std::string>> dnnPairs;
+        std::vector<std::pair<float, std::string>> baselinePairs;
+        std::vector<std::pair<float, std::string>> colorHistpairs;
+        std::vector<std::pair<float, std::string>> hist3x3Pairs;
+        std::vector<std::pair<float, std::string>> multiHistPairs;
+        std::vector<std::pair<float, std::string>> texturePairs;
+
+        // Holds target filename in a string format.
+        std::string imgBase = fs::path(imgArg).filename().string();
+
+        // Compute the similar images vector using a dnn embedding.
+        dnnPairs = dnnMatching(imgBase, dnnFilenames, dnnData);
+
+        // Reads the target image into a Mat.
+        cv::Mat target = cv::imread("olympus/" + imgBase);
+
+        // Check to ensure the image has been loaded.
+        if (target.empty()) {
+            printf("Missing Target\n");
+            return-1;
+        }
+
+        // Performs comparisons using the different feature types and stores then in a variable.
+        baselinePairs = baselineMatching(target, baselineFilenames, baselineData);
+        colorHistpairs = histMatching(target, histFilenames, histData);
+        hist3x3Pairs = hist3x3Matching(target, hist3x3Filenames, hist3x3Data);
+        multiHistPairs = multiHistMatching(target, multiHistFilenames, multiHistData);
+        texturePairs = texturedMatching(target, textureFilenames, textureData);
+
+        // Prints the filename of the target image being used.
+        printf("Target Image: %s\n", imgArg.c_str());
+
+        // Retrieves and prints the top N closest matches to the target image.
+        for (int j = 0; j < N; j++) {
+            std::string dnnOut = dnnPairs[j].second;
+            std::string baselineOut = baselinePairs[j + 1].second;
+            std::string histOut = colorHistpairs[j + 1].second;
+            std::string hist3x3Out = hist3x3Pairs[j + 1].second;
+            std::string multiOut = multiHistPairs[j + 1].second;
+            std::string texturedOut = texturePairs[j + 1].second;
 
 
+            printf("DNN:%s\tBaseline:%s\tHist:%s\tHist3x3:%s\tMulti:%s\tTexture:%s\n\n", 
+                    dnnOut.c_str(), baselineOut.c_str(), histOut.c_str(), 
+                    hist3x3Out.c_str(), multiOut.c_str(), texturedOut.c_str());
+        }
     }
 
     // Task 7: Custom License Plate Matching (texture + center color + DNN)
@@ -494,8 +676,7 @@ int main(int argc, char* argv[]){
         // Extract just the filename (e.g. "pic.0482.jpg") for DNN lookup
         std::string imgName = fs::path(imgArg).filename().string();
 
-        std::vector<std::pair<float, std::string>> pairs = customMatching(
-            targetImg, imgName, filenames, data);
+        pairs = customMatching(targetImg, imgName, filenames, data);
 
         printf("Top %d most similar:\n", N);
         printFilenames(N, pairs);
@@ -504,7 +685,20 @@ int main(int argc, char* argv[]){
         for (int i = pairs.size() - 1; i >= (int)pairs.size() - N; i--) {
             printf("%s\n", pairs[i].second.c_str());
         }
-    }
+    } 
+
+    // Extension1 - Using sum of squared differences on dnn embedding to find distances.
+    else if (method == "res2") {
+        // Computes distances and returns a pair of distances and filenames.
+        pairs = dnnSSDMatching(imgArg, filenames, data);
+        
+        // Prints the N file names with the smallest distance to target.
+        printFilenames(N, pairs);
+    } 
+
+    // Extension 2 - GUI that displays the target image N most similar images.
+    // Displays the images.
+    gui(imgArg, pairs, N);
 
     return 0;
 }
