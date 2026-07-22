d777a83d611c40731a2b246cd807117c5fdde4fa
Abhiram Banda <abhiram.ban@outlook.com>
2026-07-22 13:04:35 -0400

    added comments and cleqaned up code. saved the image we need for task1e

--- project-5-recognition-deep-networks/load_model.py
+++ project-5-recognition-deep-networks/load_model.py
@@ -1,23 +1,26 @@
 from matplotlib import pyplot as plt
 import torch
-from model import NeuralNetwork, test_data, test_dataloader
+from model import NeuralNetwork, test_data
 from torch import nn
 
+# Creates an instance of our NeuralNetwork class and loads it with the weights we saved previously
 model = NeuralNetwork() 
 model.load_state_dict(torch.load('model_weights.pth', weights_only=True))
 model.eval()
 
-# for i in range(10):
-#     img, label = test_data[i]
+# print out (on the command line) the 10 network output values (use only 2 decimal places), 
+# the index of the max output value, and the correct label of the digit.
+for i in range(10):
+    # Extracts the image and the true label from the test data
+    img, label = test_data[i]
 
+    # Predicts the the digit
+    pred = model(img.unsqueeze(0))
+    pred_digit = pred.argmax().item()
+    tensors = pred.data.tolist()[0]
+    tensors = [round(x, 2) for x in tensors]
 
-#     pred = model(img.unsqueeze(0))
-#     pred_digit = pred.argmax()
-#     tensors = pred.data.tolist()[0]
-#     tensors = [round(x, 2) for x in tensors]
-
-    
-#     print(tensors, label, pred_digit)
+    print(tensors, label, pred_digit)
 
 
 # From the example: plots the first 6 digits of the test dataset. 
@@ -26,10 +29,10 @@ cols, rows = 3, 3
 for i in range(1, 10):
     img, label = test_data[i]
     pred = model(img.unsqueeze(0))
-    pred_digit = pred.argmax()
+    pred_digit = pred.argmax().item()
 
     figure.add_subplot(rows, cols, i)
-    plt.title(pred_digit.item())
+    plt.title(f" Prediction: {pred_digit}")
     plt.axis("off")
     plt.imshow(img.squeeze(), cmap="gray")
 plt.show()
